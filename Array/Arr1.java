75aead85e545c0cb3202ec63bf143bc0fcf9f9de
Hitesh Rawekar <118650537+Minamal88@users.noreply.github.com>
2024-09-08 18:11:33 +0530

    Create Arr1.java

---
+++ Array/Arr1.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class Arr1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {10,20,30,40,50},i;
+		for(i=0;i<5;i++)
+		{
+			System.out.println("Array elements of "+i+"th positon: "+a[i]);
+		}
+	}
+
+}
