eccf0b6d1741078a8e070bc2c2a99e73866b3437
Hitesh Rawekar <118650537+Minamal88@users.noreply.github.com>
2024-09-09 11:36:33 +0530

    Create Addition1.java

---
+++ basic_Programs/Addition1.java
@@ -0,0 +1,14 @@
+package basic_Programs;
+
+public class Addition1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100,b=200,c;
+		System.out.println("Value of a: "+a+" and value of b: "+b);
+		c=a+b;
+		System.out.println("Addition is: "+c);
+		System.out.println(a+" + "+b+" = "+c);
+	}
+
+}
