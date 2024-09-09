eb64bf9026440ef7e3359e2ff97caf65c38bcd2c
Hitesh Rawekar <118650537+Minamal88@users.noreply.github.com>
2024-09-09 10:00:58 +0530

    Add files via upload

---
+++ instanceAndStaticBlock/StaticBlock.java
@@ -0,0 +1,16 @@
+package instanceAndStaticBlock;
+
+public class StaticBlock {
+	static int a=100;
+	
+	static void show()
+	{
+		System.out.println(a);
+	}
+	
+	 public static void main(String[] args) { // TODO Auto-generated method stub
+		 StaticBlock.show();
+	 }
+	 
+
+}
