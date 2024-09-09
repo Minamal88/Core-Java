d8a46c8879eef9454354a9183ac9762a96ff30bf
Hitesh Rawekar <118650537+Minamal88@users.noreply.github.com>
2024-09-09 10:47:05 +0530

    Add files via upload

---
+++ package1/Main1.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo d=new Demo();
+		d.show();
+	}
+
+}
---
+++ package1/Sum.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class Sum {
+		public void add()
+		{
+			int a=10,b=20;
+			System.out.println("Addition is:"+(a+b));
+		}
+}
