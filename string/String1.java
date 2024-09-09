c55dbbcf07031caf54c061f8c30b09bd284ffb73
Hitesh Rawekar <118650537+Minamal88@users.noreply.github.com>
2024-09-09 11:21:14 +0530

    Create String1.java

---
+++ string/String1.java
@@ -0,0 +1,16 @@
+package string;
+
+public class String1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="hello world";
+		String s1= new String("Welcome to 3RI");
+		char s2[]= {'c','a','t'};
+		String s3=new String(s2);
+		System.out.println("The first given string is: "+s);
+		System.out.println("The second given string is: "+s1);
+		System.out.println("The third given string is: "+s3);
+	}
+
+}
