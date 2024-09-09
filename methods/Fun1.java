333460132adcb145e928044e7890e9229b2243d2
Hitesh Rawekar <118650537+Minamal88@users.noreply.github.com>
2024-09-09 10:09:59 +0530

    Create Fun1.java

---
+++ methods/Fun1.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class Fun1 {
+	void fun()
+	{
+		System.out.println("hello java");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fun1 f=new Fun1();
+		//f.fun();
+		//f.fun();
+	}
+
+}
